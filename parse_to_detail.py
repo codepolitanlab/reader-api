571449fb7b1009e9e76cb7fe0724965358ee90cc
Bagus Aji Santoso <lobothijau@gmail.com>
2018-10-04 13:59:02 +0700

    Update

---
+++ parse_to_detail.py
@@ -0,0 +1,17 @@
+import json 
+import os
+
+from urllib.request import urlopen
+
+
+for x in range(1,3):
+	"""
+	Ambil 30 artikel pertama
+	"""
+	html = urlopen('http://www.codepolitan.com/api/v2/posts/latest/post/{}'.format(x)).read()
+	parsed_json = json.loads(html.decode('utf-8'))
+	for article in parsed_json['result']:
+		file = open(os.getcwd() + '/news/detail/' + '{}.json'.format(article['id']), 'w') # Buat file baru dengan nama sesuai id artikel
+		json_str_to_write = '{"code": 200, "result": ' + json.dumps(article)
+		file.write(json_str_to_write) # Isi kontennya dengan string json
+		file.close() 
