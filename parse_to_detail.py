15a1851ca6c45d6044f6999646e69103ba3e91cf
Bagus Aji Santoso <lobothijau@gmail.com>
2018-10-11 08:52:33 +0700

    Update article endpoint structure

--- parse_to_detail.py
+++ parse_to_detail.py
@@ -4,6 +4,14 @@ import os
 from urllib.request import urlopen
 
 
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+
+
 for x in range(1,4):
 	"""
 	Ambil 30 artikel pertama
@@ -11,7 +19,8 @@ for x in range(1,4):
 	html = urlopen('http://www.codepolitan.com/api/v2/posts/latest/post/{}'.format(x)).read()
 	parsed_json = json.loads(html.decode('utf-8'))
 	for article in parsed_json['result']:
-		file = open(os.getcwd() + '/article/' + '{}.json'.format(article['id']), 'w') # Buat file baru dengan nama sesuai id artikel
+		createFolder(os.getcwd() + '/article/{}'.format(article['id']))
+		file = open(os.getcwd() + '/article/' + '{}/index.json'.format(article['id']), 'w') # Buat file baru dengan nama sesuai id artikel
 		json_str_to_write = '{"code": 200, "result": ' + json.dumps(article) + '}'
 		file.write(json_str_to_write) # Isi kontennya dengan string json
 		file.close()
